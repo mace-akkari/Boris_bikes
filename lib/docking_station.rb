5158764530160871ea0a4c86e83ff364ac9c69e5
Mace <maceplus@gmail.com>
2020-10-01 12:13:07 +0100

    Add DockingStaion class

---
+++ lib/docking_station.rb
@@ -0,0 +1,20 @@
+class DockingStation
+  def release_bike
+    fail 'No bikes available' unless @bike
+    @bike
+  end
+  
+  def dock(bike)
+    @bike = bike
+  end
+
+  attr_reader :bike
+  
+end
+
+
+class Bike
+  def working?
+    true
+  end 
+end
